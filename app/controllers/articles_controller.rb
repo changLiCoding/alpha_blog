996b5f32ddc2578428c78e69ab07018546c6c0fd
javascriptsucks <evix1988@live.cn>
2022-10-30 11:29:30 +0800

    add article main page and create new article feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,5 +3,14 @@ class ArticlesController < ApplicationController
     # binding.break
     @article = Article.find(params[:id])
   end
+  def index
+    @articles = Article.all
+  end
+  def new
 
+  end
+
+  def create
+    render plain: params[:article]
+  end
 end
