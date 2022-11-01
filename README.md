3e2a85c304187a28616bb455664beb086e215710
javascriptsucks <evix1988@live.cn>
2022-11-02 00:32:26 +0800

    Add link_to data:{turbo_method: :delete turbo_confirm:} features

--- README.md
+++ README.md
@@ -5,20 +5,26 @@ application up and running.
 
 Things you may want to cover:
 
-* Ruby version
+- Ruby version
+  3.0.0
+- System dependencies
 
-* System dependencies
+- Configuration
 
-* Configuration
+- Database creation
 
-* Database creation
+- Database initialization
 
-* Database initialization
+- How to run the test suite
 
-* How to run the test suite
+- Services (job queues, cache servers, search engines, etc.)
 
-* Services (job queues, cache servers, search engines, etc.)
+- Deployment instructions
 
-* Deployment instructions
+- ...
 
-* ...
+THe frond end: Destroying (deleting) articles
+REST - Representational state transfer -mapping HTTP verbs (get, post, put/patch, delete) to CRUD actions
+
+resources provides REST-ful routes to Rails resources
+show, index, new, create, edit, update, destroy.
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,18 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
 
   def show
     @article = Article.find(params[:id])
   end
 
-  def index
-    @articles = Article.all
-  end
-
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -18,8 +21,26 @@ class ArticlesController < ApplicationController
       flash[:notice] = "Article was created successfully."
       redirect_to @article
     else
-      render 'new'
+      render :new, status: :unprocessable_entity
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article updated successfully. "
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
 
 end
