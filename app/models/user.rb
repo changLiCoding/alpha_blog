7a523103c509af703c08deaac825426a5e5fc934
javascriptsucks <evix1988@live.cn>
2022-11-08 17:13:03 +0800

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
 
+  validates :username, uniqueness: { case_sensitive: false }, presence: true, length: { in: 0..20 }
+  VALID_EMAIL_REGEX = /\A[\w+\.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, uniqueness:{ case_sensitive: false}, presence: true, length: {minimum: 6, maximun: 105}, format: {with: VALID_EMAIL_REGEX}
 end
