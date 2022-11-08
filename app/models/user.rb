3d37fdcc3fe6ef34555c0be3da1ab33515b8c495
javascriptsucks <evix1988@live.cn>
2022-11-08 20:55:39 +0800

    Add one_to_many_association between users and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+  has_many :articles
   validates :username, uniqueness: { case_sensitive: false }, presence: true, length: { in: 0..20 }
   VALID_EMAIL_REGEX = /\A[\w+\.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, uniqueness:{ case_sensitive: false}, presence: true, length: {minimum: 6, maximun: 105}, format: {with: VALID_EMAIL_REGEX}
