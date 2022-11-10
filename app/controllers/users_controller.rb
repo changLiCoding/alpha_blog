1c8ce8b8a791ee6ec2b6d0bdd879a9eef9bdaaba
javascriptsucks <evix1988@live.cn>
2022-11-11 00:45:07 +0800

    add user profile pic and articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,11 @@ class UsersController < ApplicationController
       redner :edit, status: :unprocessable_entity
     end
   end
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
 
   private
   def user_params
