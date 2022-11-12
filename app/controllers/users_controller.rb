d8d897bc994f51f700c896e62cd174116a71ec62
javascriptsucks <evix1988@live.cn>
2022-11-12 17:49:05 +0800

    build login UI with sessions controller and password authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
   end
@@ -9,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome #{@user.username} to the Alpha Blog, you have successfully sign up your account! "
       redirect_to articles_path
     else
@@ -16,10 +19,8 @@ class UsersController < ApplicationController
     end
   end
   def edit
-    @user = User.find(params[:id])
   end
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated. "
       redirect_to user_path
@@ -28,7 +29,6 @@ class UsersController < ApplicationController
     end
   end
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
@@ -37,6 +37,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-
-
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
