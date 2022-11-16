3989b7b9b030d04c55a026d5ef555f171b52cd2f
javascriptsucks <evix1988@live.cn>
2022-11-16 22:56:36 +0800

    category show edit index pages many to many association with articles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,6 +8,7 @@ class CategoriesController < ApplicationController
   end
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 3)
   end
   def index
     @categories =  Category.paginate(page: params[:page], per_page: 5)
@@ -22,6 +23,22 @@ class CategoriesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  def edit
+    @category = Category.find(params[:id])
+  end
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      flash[:notice] = "Category updated successfully. "
+      redirect_to @category
+    else
+      render :edit, status: :unprocessable_entity
+
+    end
+  end
+
+
 
   private
   def category_params
